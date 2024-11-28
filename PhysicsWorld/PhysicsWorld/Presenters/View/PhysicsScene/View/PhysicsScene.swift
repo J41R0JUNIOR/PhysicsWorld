57ea3ed75be69c086b9e96fa757070de1c744d9f
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-11-28 11:09:05 -0300

    cleaning code

--- PhysicsWorld/PhysicsWorld/Presenters/View/PhysicsScene/View/PhysicsScene.swift
+++ PhysicsWorld/PhysicsWorld/Presenters/View/PhysicsScene/View/PhysicsScene.swift
@@ -32,7 +32,6 @@ class PhysicsScene: UIViewController {
     }
     
     func update(_ currentTime: TimeInterval){
-//        calculatePhysics()
         for n in objects {
             n.update(deltatime: currentTime)
             
@@ -44,41 +43,6 @@ class PhysicsScene: UIViewController {
         self.view.addSubview(circleView)
         self.objects.append(circleView)
     }
- 
-//    func calculatePhysics() {
-//        for i in objects.indices {
-//            let object = objects[i]
-//            
-//            guard object.isDynamic else { continue }
-//            
-//            let response = applyintGravity(for: object)
-//            objects[i].velocity = response.velocity
-//            objects[i].center = response.newPosition
-//        }
-//    }
-
-//    func applyintGravity(for object: UIView & ObjectsProtocol) -> (velocity: CGPoint, newPosition: CGPoint) {
-//        let gravity = CGPoint(x: 0, y: 9.81)
-//        let deltaTime = deltaTime
-//        
-//        let newVelocity = CGPoint(
-//            x: object.velocity.x + gravity.x * object.mass * CGFloat(deltaTime),
-//            y: object.velocity.y + gravity.y * object.mass * CGFloat(deltaTime)
-//        )
-//        
-//        let newPosition = CGPoint(
-//            x: object.center.x + newVelocity.x * CGFloat(deltaTime),
-//            y: object.center.y + newVelocity.y * CGFloat(deltaTime)
-//        )
-//        
-//        if object.layer.position.y > self.view.bounds.maxY + object.layer.frame.height {
-//            let initialPosition = CGPoint(x: object.layer.position.x, y: .zero - object.layer.frame.height)
-//            
-//            return (velocity: .zero, newPosition: initialPosition)
-//        }
-//        
-//        return (velocity: newVelocity, newPosition: newPosition)
-//    }
 }
 
 #Preview {

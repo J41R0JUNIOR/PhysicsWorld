0f20ec4246f81e91947c3c83e3fed514ef64ba0d
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-12-02 16:20:35 -0300

    fixed the button layers that didn't allow to be touched

--- PhysicsWorld/PhysicsWorld/Presenters/View/HUDPhysicsScene/Extension/Ex_ViewCode.swift
+++ PhysicsWorld/PhysicsWorld/Presenters/View/HUDPhysicsScene/Extension/Ex_ViewCode.swift
@@ -14,23 +14,30 @@ extension HUDPhysicsScene: ViewCode {
             qtdNodesLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             
             resetButton.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 10),
-            resetButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            resetButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            editModeToggle.leadingAnchor.constraint(equalTo: resetButton.trailingAnchor, constant: 10),
+            editModeToggle.centerYAnchor.constraint(equalTo: resetButton.centerYAnchor)
         ])
     }
     
     func addViews() {
         view.addSubview(qtdNodesLabel)
         view.addSubview(resetButton)
-        
-//        addChild(physicsScene)
+        view.addSubview(editModeToggle)
         view.addSubview(physicsScene.view)
+        
+//        view.addSubview(resetButton)
+//        view.addSubview(editModeToggle)
+        view.bringSubviewToFront(resetButton)
+        view.bringSubviewToFront(editModeToggle)
     }
     
     func setupStyle() {
         qtdNodesLabel.textAlignment = .center
         qtdNodesLabel.frame = view.bounds
         qtdNodesLabel.translatesAutoresizingMaskIntoConstraints = false
-        qtdNodesLabel.backgroundColor = .white
+        qtdNodesLabel.backgroundColor = .systemBackground
         
         resetButton.setTitle("Reset", for: .normal)
         resetButton.setTitleColor(.systemBackground, for: .normal)
@@ -44,12 +51,17 @@ extension HUDPhysicsScene: ViewCode {
         physicsScene.view.frame = view.bounds
         physicsScene.view.backgroundColor = .clear
         physicsScene.didMove(toParent: self)
-        
         physicsScene.view.layer.borderWidth = 10
         physicsScene.view.layer.borderColor = UIColor.blue.cgColor
         physicsScene.view.layer.bounds.size = .init(width: 4000, height: 4000)
         
-        
+        editModeToggle.setTitle("Edit", for: .normal)
+        editModeToggle.setTitleColor(.systemBackground, for: .normal)
+        editModeToggle.backgroundColor = .label
+        editModeToggle.layer.cornerRadius = 10
+        editModeToggle.layer.zPosition = 1
+        editModeToggle.addTarget(self, action: #selector(editMode), for: .touchUpInside)
+        editModeToggle.translatesAutoresizingMaskIntoConstraints = false
     }
 }
 
--- PhysicsWorld/PhysicsWorld/Presenters/View/HUDPhysicsScene/View/HUDPhysicsScene.swift
+++ PhysicsWorld/PhysicsWorld/Presenters/View/HUDPhysicsScene/View/HUDPhysicsScene.swift
@@ -9,30 +9,39 @@ import Foundation
 import UIKit
 import SwiftUI
 
-class HUDPhysicsScene: UIViewController {
-
+class HUDPhysicsScene: UIViewController, Updateable {
+    var timerToUpdate: Timer?
+    
+    var deltaTime: TimeInterval = 1/2
+    
     let physicsScene = PhysicsScene()
     
     var qtdNodes: Int = 0
     let qtdNodesLabel: UILabel = .init()
     let resetButton = UIButton()
+    var editModeToggle: UIButton = .init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViewCode()
-       
+        startUpdateLoop(deltaTime: deltaTime)
     }
     
     @objc func reset() {
         for n in physicsScene.objects {
-
+            
             n.removeFromSuperview()
             physicsScene.objects.removeAll(where: { $0 === n })
+            physicsScene.view.layer.sublayers?.removeAll(where: { $0 is CAShapeLayer })
         }
     }
     
-    func update() {
-        self.qtdNodes = self.view.subviews.count
+    @objc func editMode() {
+        physicsScene.editMode.toggle()
+    }
+    
+    func update(_ deltaTime: TimeInterval) {
+        self.qtdNodes = physicsScene.view.subviews.count
         qtdNodesLabel.text = "qtd nodes: \(qtdNodes)"
     }
 }
